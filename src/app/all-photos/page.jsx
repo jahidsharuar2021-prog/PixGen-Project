86669068d9fb4763614878cff6edde748818aab6
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-06-15 01:15:48 +0600

    all photos page added

---
+++ src/app/all-photos/page.jsx
@@ -0,0 +1,20 @@
+import PhotoCard from "@/components/PhotoCard";
+
+
+const AllPhotoPage =async () => {
+    const res = await fetch("https://pix-gen-project-tau.vercel.app/data.json");
+    const photos=await res.json()
+    console.log(photos);
+    return (
+      <div>
+        <h1 className="text-2xl font-bold m-4">All photos</h1>
+        <div className="grid grid-cols-4 gap-5">
+          {photos.map((photo) => (
+            <PhotoCard key={photo.id} photo={photo} />
+          ))}
+        </div>
+      </div>
+    );
+};
+
+export default AllPhotoPage;
