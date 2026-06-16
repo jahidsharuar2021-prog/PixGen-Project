097d5891f1052751b9d0176e781d293e63a63594
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-06-17 01:21:21 +0600

    Final-project

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,15 +1,23 @@
+import Category from "@/components/Category";
 import PhotoCard from "@/components/PhotoCard";
 
 
-const AllPhotoPage =async () => {
+const AllPhotoPage =async ({searchParams}) => {
+
+  const {category}= await searchParams;
+  // console.log(category);
+
     const res = await fetch("https://pix-gen-project-tau.vercel.app/data.json");
     const photos=await res.json()
+
+    const filteredPhotos= category? photos.filter(photo=>photo.category.toLowerCase()==category.toLowerCase()) :photos
     // console.log(photos);
     return (
       <div>
         <h1 className="text-2xl font-bold m-4">All photos</h1>
+        <Category></Category>
         <div className="grid grid-cols-4 gap-5">
-          {photos.map((photo) => (
+          {filteredPhotos.map((photo) => (
             <PhotoCard key={photo.id} photo={photo} />
           ))}
         </div>
